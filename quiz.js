61130567b3758871cc0ef1a58b56680d5f2155f1
Peter-White <pwhite007@icloud.com>
2015-05-01 13:57:39 -0400

    Quiz completed?

--- quiz.js
+++ quiz.js
@@ -14,6 +14,12 @@
 // have waiting lists, and adds herself to the waiting list for each
 // book.
 
+Attribute:            Method:
+
+user                  view datatbase
+catalog               check waitlist
+books                 apply user to waitlist per book
+
 // Question 2
 
 // You are writing a recipe database!  Write a constructor for a
@@ -24,7 +30,12 @@
 
 // (your code here)
 
-
+var Recipe = function(name, category, preparation, time) {
+  this.name = name;
+  this.category = category;
+  this.preparation = preparation;
+  this.time = time;
+}
 
 // Question 3:
 
@@ -40,7 +51,9 @@
 // 3. Iced Cold-Brew Irish Coffee, a beverage, which takes 10 minutes of
 //    preparation and 24 hours total time
 
-
+var chocolateCake = new Recipe("Grandmother's Chocolate Cake", "dessert", "2 hours", "12 hours");
+var tuneSuprise = new Recipe("Tuna Surprise Surprise", "casserole", "45 minutes", "45 minutes");
+var irishCoffee = new Recipe("Iced Cold-Brew Irish Coffee", "beverage", "10 minutes", "24 hours");
 
 // Question 4
 
@@ -58,3 +71,17 @@
 
 var validRecipeCategories = ['none', 'dessert', 'casserole',
     'salad', 'beverage', 'appetizer', 'main course', 'snack'];
+
+Recipe.prototype.getRecipeName = function () {
+  return this.name;
+};
+
+Recipe.prototype.setRecipeName = function (newRecipeName) {
+  this.name = newRecipeName;
+};
+
+Recipe.prototype.setRecipeCategory = function (newRecipeCategory) {
+  if (validRecipeCategories.indexOf(newRecipeCategory) !== -1) {
+    this.category = newRecipeCategory;
+  }
+};
