b9c504bd5d352c241c2f8fdd90981adbdea9f593
astoellis <astoellis@gmail.com>
2015-05-01 13:54:23 -0400

    solved satisfactorily

--- quiz.js
+++ quiz.js
@@ -14,6 +14,27 @@
 // have waiting lists, and adds herself to the waiting list for each
 // book.
 
+// Objects:
+//   1) LibraryMember
+//   2) Catalog
+//   3) Book
+
+// Attributes:
+//   LibraryMember-
+//     1) currentlyCheckedOut
+//     2) currentlyWaiting
+//     3) duesOwed
+//     4) address
+//     5) libraryCardNumber
+
+// Methods:
+//   1) checkOutBook(book)
+//   2) turnedInBook(book)
+//   3) changeOfAddress(newAddress)
+//   4) notifyOutstandingLateFees
+//   5) sendDueDateReminder
+
+
 // Question 2
 
 // You are writing a recipe database!  Write a constructor for a
@@ -23,6 +44,11 @@
 // Times are human-readable strings like "2 hours" "3 days" "15 minutes"
 
 // (your code here)
+var Recipe = function(name, category, preparationTime, totalTime){
+  this.name = name;
+  this.prepTime = preparationTime;
+  this.totalTime = totalTime
+}
 
 
 
@@ -34,12 +60,16 @@
 // 1. Grandmother's Chocolate Cake, a dessert, which takes 2 hours of
 //    preparation and 12 hours total time
 
+var grandmaChocoCake = new Recipe("Grandmother's Chocolate Cake", 'dessert', '2 hours', '12 hours');
+
 // 2. Tuna Surprise Surprise, a casserole, which takes 45 minutes of
 //    preparation and 45 minutes total time
 
+var tunasurprise = new Recipe('Tuna Surprise Surprise', 'casserole', '45 minutes', '45 minutes');
+
 // 3. Iced Cold-Brew Irish Coffee, a beverage, which takes 10 minutes of
 //    preparation and 24 hours total time
-
+var irishCoffee = new Recipe('Iced Cold-Brew Irish Coffee', 'beverage', '10 minutes', '24 hours');
 
 
 // Question 4
@@ -48,13 +78,31 @@
 // described.  If the user passes in a value that is not valid, write
 // a log message but do not change the object attributes.
 
-// getRecipeName
+Recipe.prototype.getRecipeName = function(){
+  return this.name;
+}
 
 // setRecipeName - once a recipe has a name, it cannot be set to the
 // empty string.
 
+Recipe.prototype.setRecipeName = function(newName) {
+  if(newName !== undefined){
+    this.name = newName;
+  } else {
+    console.log('You entered an invalid name');
+  }
+};
+
 // setRecipeCategory - the only acceptable recipe categories are given
 // in the validRecipeCategory array.
+
+Recipe.prototype.setRecipeCategory = function(newCategory) {
+  if(validRecipeCategories.indexOf(newCategory) >= 0){
+      this.category = newCategory;
+    } else {
+      console.log('You entered an invalid categoryf');
+    }
+};
 
 var validRecipeCategories = ['none', 'dessert', 'casserole',
     'salad', 'beverage', 'appetizer', 'main course', 'snack'];
